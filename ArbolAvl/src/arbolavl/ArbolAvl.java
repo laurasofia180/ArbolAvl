f86bdb4d41cd27d874a4194fa352c107986f72df
laurasofi180 <muozmontoyal@gmail.com>
2014-05-29 17:46:48 -0500

    Se mejora la clase nodo pero se sube con unerror

--- ArbolAvl/src/arbolavl/ArbolAvl.java
+++ ArbolAvl/src/arbolavl/ArbolAvl.java
@@ -11,13 +11,5 @@ package arbolavl;
  * @author sofia
  */
 public class ArbolAvl {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       // ArbolAvl Avl = new ArbolAvl ();
-        //Avl.insertar();
-        // TODO code application logic here
-    }
+    
 }
--- ArbolAvl/src/arbolavl/Nodo.java
+++ ArbolAvl/src/arbolavl/Nodo.java
@@ -3,99 +3,154 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package arbolavl;
 
 /**
  *
  * @author sofia
  */
-public class Nodo {
-    int dato;
-    Nodo dere,izq;
-    Nodo raiz;
-    public void ArbolAvl(){
-        raiz=null;
-    }
-    
-    public void insertar(int dato){   // Inserta o el nuevo dato en un nodo diferente segun lo que sea indicado 
-        Nodo nuevo;
-        nuevo = new Nodo();
-        nuevo.dato = dato;
-        nuevo.dere = null;
-        nuevo.izq = null;
-        if (raiz == null){
-            raiz = nuevo;
-        }else{
-            Nodo ant = null,recorre;
-            recorre = raiz;
-            while(recorre != null){
-                ant = recorre;
-                if (dato < recorre.dato){
-                   recorre = recorre.dere;
-       
-                }else {
-                 recorre = recorre.izq;
-                }
-                
-               if (dato < ant.dato){
-                   ant.dere = nuevo;
- 
-               } else {
-                   ant.izq = nuevo;
-               }
- 
-            }
+class Nodo implements Comparable{
 
+    int dato;
+    Nodo dere, izq, raiz;
+
+    public Nodo(int dato, Nodo dere, Nodo izq, Nodo raiz) {
+        this.dato = dato;
+        this.dere = dere;
+        this.izq = izq;
+        this.raiz = raiz;
+    }
+
+    public boolean esHoja() {
+        return ((izq == null) && (dere == null));
+    }
+
+    public int izqPeso() {
+        if (izq == null) {
+            return 0;
+        } else {
+            return izq.peso();
         }
     }
-    private void  ImprimePreOrden(Nodo recorre)
-      {
-          if (recorre != null)
-          {
-              System.out.print(recorre.dato + " ");
-              ImprimePreOrden (recorre.izq);
-              ImprimePreOrden (recorre.dere);
-          }
-      }
+
+    public int derePeso() {
+        if (dere == null) {
+            return 0;
+        } else {
+            return dere.peso();
+        }
+    }
+
+    public int peso() {
+        return 1 + Math.max(izqPeso(), derePeso());
+    }
+
+    public int profundidad() {
+        if (raiz == null) {
+            return 0;
+        } else {
+            return 1 + raiz.profundidad();
+        }
+    }
+
+    public boolean balanceo() {
+        if (esHoja()) {
+            return true;
+        } else {
+            boolean izqbalanceo = (izq == null || izq.balanceo());
+            boolean derebalanceo = (dere == null || dere.balanceo());
+            boolean currentbalanceo = (Math.abs(derePeso() - izqPeso()) <= 1);
+
+            return currentbalanceo && izqbalanceo && derebalanceo;
+        }
+    }
     
-    public void ImprimePreOrden ()
-      {
-          ImprimePreOrden (raiz);
-          System.out.println();
-      }
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public int getDato() {
+        return dato;
+    }
     
-       private void imprimeEnOrden (Nodo recorre)
-      {
-          if (recorre != null)
-          {    
-              imprimeEnOrden (recorre.izq);
-              System.out.print(recorre.dato + " ");
-              imprimeEnOrden (recorre.dere);
-          }
-      }
+    public void setRaiz(Nodo raiz){
+        this.raiz = raiz;
+        if (this.izq != null && this.izq.raiz != this){
+            this.izq.setRaiz(this);
+        }
+        if (this.dere != null && this.dere.raiz != this){
+            this.dere.setRaiz(this);
+        }
+    }
 
-      public void imprimeEnOrden ()
-      {
-          imprimeEnOrden (raiz);
-          System.out.println();
-      }
+    public void setDere(Nodo dere) {
+        if (!((dere == null) || (dato.compareTo(dere.getDato()) == -1))) {
+            throw new RuntimeException("No se puede poner el " + dere.dato + " en la derecha " + dato);
+        }
 
+        this.dere = dere;
 
-      private void imprimePostOrden (Nodo recorre)
-      {
-          if (recorre != null)
-          {
-              imprimePostOrden (recorre.izq);
-              imprimePostOrden (recorre.dere);
-              System.out.print(recorre.dato + " ");
-          }
-      }
+        if (this.dere != null) {
+            this.dere.setRaiz(this);
+        }
+    }
+    
+    public void setIzq(Nodo izq) {
+        if (!((izq == null) || (dato.compareTo(izq.getDato()) == 1))) {
+            throw new RuntimeException("No se puede poner el " + izq.dato + " en la izquierda " + dato);
+        }
 
+        this.izq = izq;
 
-      public void imprimePostOrden ()
-      {
-          imprimePostOrden (raiz);
-          System.out.println();
-      }
+        if (this.izq != null) {
+            this.izq.setRaiz(this);
+        }
+    }
+    
+    // Este metodo se encarga de llamar al método recursivo pasando la dirección del nodo raiz.Visitar la raiz.
+    //- Recorrer el subárbol izquierdo en pre-orden.
+    //- Recorrer el subárbol derecho en pre-orden.
+    private void ImprimePreOrden(Nodo recorre) {
+        if (recorre != null) {
+            System.out.print(recorre.dato + " ");
+            ImprimePreOrden(recorre.izq);
+            ImprimePreOrden(recorre.dere);
+        }
+    }
+
+    public void ImprimePreOrden() {
+        ImprimePreOrden(raiz);
+        System.out.println();
+    }
+
+    private void imprimeEnOrden(Nodo recorre) {
+        if (recorre != null) {
+            imprimeEnOrden(recorre.izq);
+            System.out.print(recorre.dato + " ");
+            imprimeEnOrden(recorre.dere);
+        }
+    }
+
+    public void imprimeEnOrden() {
+        imprimeEnOrden(raiz);
+        System.out.println();
+    }
+
+    private void imprimePostOrden(Nodo recorre) {
+        if (recorre != null) {
+            imprimePostOrden(recorre.izq);
+            imprimePostOrden(recorre.dere);
+            System.out.print(recorre.dato + " ");
+        }
+    }
+
+    public void imprimePostOrden() {
+        imprimePostOrden(raiz);
+        System.out.println();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
